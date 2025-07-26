0f21df7c10a4f55e49405e6ad3c07f8644112e21
Piyush Das <piyushdas3110@gmail.com>
2025-07-26 14:40:48 +0000

    feat(modules): add index files for module exports

---
+++ src/app/dashboard/affiliate-showcase/index.ts
@@ -0,0 +1,16 @@
+// Components
+export { default as AffiliateShowcaseContainer } from './components/AffiliateShowcaseContainer';
+export { default as FilterTabs } from './components/FilterTabs';
+export { default as SearchBar } from './components/SearchBar';
+export { default as ProductCard } from './components/ProductCard';
+export { default as ProductGrid } from './components/ProductGrid';
+export { default as LoadingState } from './components/LoadingState';
+
+// Store
+export { useAffiliateShowcaseStore } from './store/useAffiliateShowcaseStore';
+
+// Types
+export type { AffiliateProduct, FilterType, AffiliateShowcaseStore } from './types';
+
+// Constants
+export { MOCK_AFFILIATE_PRODUCTS, AFFILIATE_CATEGORIES, AFFILIATE_COLORS } from './constants';
